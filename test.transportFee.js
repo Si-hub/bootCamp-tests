03b0414966c77edb4a4bf98676c98d4a930b0c61
Si-hub <zweto.dayile@gmail.com>
2020-04-02 17:16:55 +0200

    added a text to a file

---
+++ test.transportFee.js
@@ -0,0 +1,17 @@
+describe("The transportFee function ", function(){
+  
+    it ("should return R20 if its a morning shift", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal("R20", transportFee("morning"));
+    }); 
+  
+  it ("should return R10 if its a afternoon shift ", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal("R10", transportFee("afternoon"));
+    }); 
+  
+  it ("should return free if its a night shift", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal("free", transportFee("free"));
+    }); 
+});
